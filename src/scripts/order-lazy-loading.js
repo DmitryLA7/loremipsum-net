70a68475753ad85ddb4090852def6f93f42b6f70
DmitryLA7 <dmitriy.lavrov.adci@gmail.com>
2023-05-31 13:49:51 +0600

    bugs fixes

--- src/scripts/order-lazy-loading.js
+++ src/scripts/order-lazy-loading.js
@@ -1,13 +1,12 @@
 const orderLazyLoading = () => {
   const element = document.querySelector("#order");
-  const options = {
-    threshold: 0.5,
-  };
+  const options = { threshold: [0.25, 0.5] };
 
   const showAnimation = (entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add("init-anim");
+        console.log("123");
         observer.unobserve(entry.target);
       }
     });
